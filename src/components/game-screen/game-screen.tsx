75b67a692e9b24cced18f63c4810b674d8ae2b93
Matt Newbigging <mrnewbigging@gmail.com>
2025-06-22 14:43:28 +0100

    todo

--- src/components/game-screen/game-screen.tsx
+++ src/components/game-screen/game-screen.tsx
@@ -1,22 +1,10 @@
-import { appState } from "../../app-state/app-state";
-import { BotGame, GameStage, RPS } from "../../app-state/bot-game";
+import { BotGame, GameStage } from "../../app-state/bot-game";
 import { useEventUpdater } from "../../hooks/use-event-updater";
-import "./game-screen.scss";
 import { RpsPicker } from "./rps-picker/rps-picker";
 
 interface GameScreenProps {
   game: BotGame;
 }
-
-/**
- * Game comp flow:
- *
- * 1 - show RPS choice buttons, player picks one
- *
- * 2 - choice moves to center of screen
- *
- * 3 - when opponent choice is made, show it below yours with result string
- */
 
 export function GameScreen({ game }: GameScreenProps) {
   useEventUpdater("show-choices");
